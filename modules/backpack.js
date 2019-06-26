e8195c3e1b2590ce3f54264b151e36da08775614
segalll <hseg1227@gmail.com>
2019-06-26 08:55:46 -0500

    Add backpack module

---
+++ modules/backpack.js
@@ -0,0 +1,53 @@
+const mysql = require('./mysql.js');
+
+exports.addItem = (user, item) => {
+	let data = await mysql.getUserData(user.id, 'backpack');
+	let backpack = data.backpack.split(',');
+	backpack.push(item);
+	mysql.setUserData(user.id, `backpack = ${backpack.join(',')}`);
+	return 1;
+}
+
+exports.removeItem = (user, item) => {
+	let data = await mysql.getUserData(user.id, 'backpack');
+	let backpack = data.backpack.split(',');
+	if (backpack.includes(item)) {
+		backpack = backpack.filter(value => value != item);
+		mysql.setUserData(user.id, `backpack = ${backpack.join(',')}`);
+		return 1;
+	}
+	return 0;
+}
+
+exports.getBackpack = user => {
+	let data = await mysql.getUserData(user.id, 'backpack');
+	return data.backpack.split(',');
+}
+
+exports.getInventory = user => {
+	let data = await mysql.getUserData(user.id, 'inventory');
+	return data.inventory.split(',');
+}
+
+exports.equip = (user, item) => {
+	let data = await mysql.getUserData(user.id, 'backpack, inventory');
+	let backpack = data.backpack.split(',');
+	let inventory = data.inventory.split(',');
+	if (backpack.includes(item)) {
+		inventory.push(item);
+		mysql.setUserData(user.id, `inventory = ${inventory.join(',')}`);
+		return 1;
+	}
+	return 0;
+}
+
+exports.unequip = (user, item) => {
+	let data = await mysql.getUserData(user.id, 'inventory');
+	let inventory = data.inventory.split(',');
+	if (inventory.includes(item)) {
+		inventory = inventory.filter(value => value != item);
+		mysql.setUserData(user.id, `inventory = ${inventory.join(',')}`);
+		return 1;
+	}
+	return 0;
+}
