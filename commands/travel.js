b398cc5c80a320867b3acacc39d8664200c546a1
Tejas Tank <tejastank10@gmail.com>
2019-06-29 12:23:18 +0530

    Fixed travel command

--- commands/travel.js
+++ commands/travel.js
@@ -6,8 +6,8 @@ module.exports.run = async(client, message, args) => {
 	if(!cities[args[0]]) {
         await message.channel.send(`City by the name of ${args[0]} does not exist!`);
     } else {
-        let travelTime = await traveljs.travelTo(player, args[0]);
-        await message.channel.send(`Now travelling to ${args[0]}. You will arrive in ${travelTime} hours.`);
+        let travelTime = await traveljs.travelTo(message.author, args[0]);
+        await message.channel.send(`Now travelling to ${args[0]}. You will arrive in ${travelTime} minutes.`);
     }
 }
 
